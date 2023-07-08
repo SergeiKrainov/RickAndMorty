aad3bee9aaacba1170b4d6f93fd7cd7f06ffc3a7
SergeiKrainov <89527271+SergeiKrainov@users.noreply.github.com>
2023-07-08 13:27:43 +0300

    Wrap up location pagination + device families

--- RickAndMorty/VIewModels/Location/RMLocationViewViewModel.swift
+++ RickAndMorty/VIewModels/Location/RMLocationViewViewModel.swift
@@ -28,9 +28,63 @@ final class RMLocationViewViewModel {
     
     private var apiInfo: RMGetAllLocationResponse.Info?
     
+    private var didFinishPagination: (() -> Void)?
+    
     public private(set) var cellViewModels: [RMLocationTableViewCellViewModel] = []
     
+    public var shouldShowLoadMoreIndicator: Bool {
+        return apiInfo?.next != nil
+    }
+    
+    public var isLoadingMoreLocation = false
+    
+    //MARK: - Init
+    
     init() {}
+    
+    //MARK: - Public
+    
+    public func registerDidFinishPaginationBlock(_ block: @escaping () -> Void) {
+        self.didFinishPagination = block
+    }
+    
+    /// Paginate if additional locations are needed
+    public func fetchAdditionalLocations() {
+        guard !isLoadingMoreLocation else {return}
+        
+        guard let nextUrlString = apiInfo?.next,
+              let url = URL(string: nextUrlString) else {return}
+        
+        isLoadingMoreLocation = true
+        
+        guard let request = RMRequest(url: url) else {
+            isLoadingMoreLocation = false
+            return
+        }
+        
+        RMService.shared.execute(request,
+                                 expecting: RMGetAllLocationResponse.self) { [weak self] result in
+            guard let self = self else {return}
+            switch result {
+            case .success(let responseModel):
+                let moreResults = responseModel.results
+                let info = responseModel.info
+                self.apiInfo = info
+                self.cellViewModels.append(contentsOf: moreResults.compactMap({
+                    return RMLocationTableViewCellViewModel(location: $0)
+                }))
+                
+                DispatchQueue.main.async {
+                    self.isLoadingMoreLocation = false
+                    // Notify via callback
+                    self.didFinishPagination?()
+                }
+            case .failure(let failure):
+                print(String(describing: failure))
+                isLoadingMoreLocation = false
+            }
+        }
+    }
     
     public func location(at index: Int) -> RMLocation? {
         guard index < locations.count, index >= 0 else {
@@ -49,7 +103,7 @@ final class RMLocationViewViewModel {
                 DispatchQueue.main.async {
                     self?.delegate?.didFetchInitialLocation()
                 }
-            case .failure(let failure):
+            case .failure:
                 break
             }
         }
